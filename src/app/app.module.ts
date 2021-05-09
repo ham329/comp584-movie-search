60097c3303c5a32e48183dc2eccc705644fd95b4
cindynavarreteteo <cindy.navarreteteo.671@my.csun.edu>
2021-05-08 21:45:24 -0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
+import { FavoriteListComponent } from './favorite-list/favorite-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, MovieSearchComponent, MovieDashboardComponent, ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MovieSearchComponent,
+    MovieDashboardComponent,
+    FavoriteListComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
