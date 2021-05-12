c4a3fe5e0bbbcc7a367f9a6d94ba2fc28c522629
jvsooon <jasonsabal600@gmail.com>
2021-05-12 00:12:59 -0700

    style: Switch from light to dark theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,33 +9,51 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SlickModule } from 'ngx-slick';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
-// <<<<<<< dev-test
 import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, MatButtonModule, MatInputModule, MatIconModule, MatTabsModule, ReactiveFormsModule, BrowserAnimationsModule ],
-  declarations: [ AppComponent, HelloComponent, MovieSearchComponent, MovieDashboardComponent, AuthComponent, ],
-  bootstrap:    [ AppComponent ]
-// =======
-// import { FavoriteListComponent } from './favorite-list/favorite-list.component';
-// import { LoginComponent } from './login/login.component';
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    SlickModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    MovieSearchComponent,
+    MovieDashboardComponent,
+    AuthComponent
+  ],
+  bootstrap: [AppComponent]
+  // =======
+  // import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+  // import { LoginComponent } from './login/login.component';
 
-// @NgModule({
-//   imports: [BrowserModule, FormsModule, HttpClientModule],
-//   declarations: [
-//     AppComponent,
-//     HelloComponent,
-//     MovieSearchComponent,
-//     MovieDashboardComponent,
-//     FavoriteListComponent,
-//     LoginComponent
-//   ],
-//   bootstrap: [AppComponent]
-// >>>>>>> master
+  // @NgModule({
+  //   imports: [BrowserModule, FormsModule, HttpClientModule],
+  //   declarations: [
+  //     AppComponent,
+  //     HelloComponent,
+  //     MovieSearchComponent,
+  //     MovieDashboardComponent,
+  //     FavoriteListComponent,
+  //     LoginComponent
+  //   ],
+  //   bootstrap: [AppComponent]
+  // >>>>>>> master
 })
 export class AppModule {}
