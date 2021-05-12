394d203a207f61ec83be55f5ad6b5425d3e1a856
jvsooon <jasonsabal600@gmail.com>
2021-05-11 19:03:17 -0700

    feat: Setup app routing to navigate between login and dashboard and add login/register form to login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MovieSearchComponent } from './movie-search/movie-search.component';
 import { MovieDashboardComponent } from './movie-dashboard/movie-dashboard.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, MovieSearchComponent, MovieDashboardComponent, ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, MatButtonModule, MatInputModule, MatIconModule, MatTabsModule, ReactiveFormsModule, BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent, MovieSearchComponent, MovieDashboardComponent, AuthComponent, ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
