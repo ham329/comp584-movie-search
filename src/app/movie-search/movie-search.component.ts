813e3355554927dde89f6c31e63f2160089d2ceb
ham329 <aaronmartinhabana@gmail.com>
2021-05-19 16:53:36 -0700

    3 files modified

--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-movie-search',
   templateUrl: './movie-search.component.html',
   styleUrls: ['./movie-search.component.css']
 })
-export class MovieSearchComponent {
+export class MovieSearchComponent implements OnInit {
 
   constructor(private _httpClient: HttpClient) {}
 
@@ -21,6 +20,10 @@ export class MovieSearchComponent {
       })
   }
  
+ slideConfig = { slidesToShow: 5, slidesToScroll: 1, dots: false };
+  afterChange(e) {
+    console.log('afterChange');
+  }
 
   ngOnInit() {
     
